a1cb02d11ab1bc6aa8e5a25b3a9ef8c55364d166
osatis <osatis@osatis.osatis>
2022-02-24 11:13:38 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HCode22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=========== HCode22 BEGIN ===========");
+
+            _ = new Solver("inputs/a_an_example.in.txt",  "a.out", ' ');
+            _ = new Solver("inputs/b_basic.in.txt",       "b.out", ' ');
+            _ = new Solver("inputs/c_coarse.in.txt",      "c.out", ' ');
+            _ = new Solver("inputs/d_difficult.in.txt",   "d.out", ' ');
+            _ = new Solver("inputs/e_elaborate.in.txt",   "e.out", ' ');
+
+            Console.WriteLine("=========== HCode22 END =============");
+        }
+    }
+}
